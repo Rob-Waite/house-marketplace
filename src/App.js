04cf3f7568e8a6be7f4938c75019a9018eddfc19
Rob <rgwwebdesign@gmail.com>
2022-04-10 12:35:23 +0100

    Add User Sign In Functionality

--- src/App.js
+++ src/App.js
@@ -12,17 +12,17 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<Explore />} />
-          <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<SignIn />} />
-          <Route path='/sign-in' element={<SignIn />} />
-          <Route path='/sign-up' element={<SignUp />} />
-          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
         <Navbar />
       </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
