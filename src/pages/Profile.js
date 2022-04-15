cb115e626b6cc7e3afa44fcc147420a8f19754e2
Rob <rgwwebdesign@gmail.com>
2022-04-15 18:11:44 +0100

    Create Listing

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import { getAuth, updateProfile } from 'firebase/auth'
 import { updateDoc, doc } from 'firebase/firestore'
 import { db } from '../firebase.config'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
 
 function Profile() {
   const auth = getAuth()
@@ -47,19 +49,19 @@ function Profile() {
   }
 
   return (
-    <div className="profile">
-      <header className="profileHeader">
-        <p className="pageHeader">My Profile</p>
-        <button type="button" className="logOut" onClick={onLogout}>
+    <div className='profile'>
+      <header className='profileHeader'>
+        <p className='pageHeader'>My Profile</p>
+        <button type='button' className='logOut' onClick={onLogout}>
           Logout
         </button>
       </header>
 
       <main>
-        <div className="profileDetailsHeader">
-          <p className="profileDetailsText">Personal Details</p>
+        <div className='profileDetailsHeader'>
+          <p className='profileDetailsText'>Personal Details</p>
           <p
-            className="changePersonalDetails"
+            className='changePersonalDetails'
             onClick={() => {
               changeDetails && onSubmit()
               setChangeDetails((prevState) => !prevState)
@@ -69,19 +71,19 @@ function Profile() {
           </p>
         </div>
 
-        <div className="profileCard">
+        <div className='profileCard'>
           <form>
             <input
-              type="text"
-              id="name"
+              type='text'
+              id='name'
               className={!changeDetails ? 'profileName' : 'profileNameActive'}
               disabled={!changeDetails}
               value={name}
               onChange={onChange}
             />
             <input
-              type="text"
-              id="email"
+              type='text'
+              id='email'
               className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
               disabled={!changeDetails}
               value={email}
@@ -89,6 +91,12 @@ function Profile() {
             />
           </form>
         </div>
+
+        <Link to='/create-listing' className='createListing'>
+          <img src={homeIcon} alt='home' />
+          <p>Sell or rent your home</p>
+          <img src={arrowRight} alt='right arrow' />
+        </Link>
       </main>
     </div>
   )
